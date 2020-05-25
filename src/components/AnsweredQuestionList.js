de01ab9ebb97c06728b1f8d975a21858ebc58b0a
Elena Lambova <elena.g.lambova@gmail.com>
2020-05-25 16:45:32 +0300

    Feat: Add AnsweredQuestionList.js Component.

---
+++ src/components/AnsweredQuestionList.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import Question from "./Question";
+
+class AnsweredQuestionList extends Component {
+  render() {
+    const { authUser, questions, questionId } = this.props;
+    return (
+      <Fragment>
+        {questionId.map((id) =>
+          questions[id].optionOne.votes
+            .concat(questions[id].optionTwo.votes)
+            .includes(authUser) ? (
+            <Question key={id} id={id} />
+          ) : null
+        )}
+      </Fragment>
+    );
+  }
+}
+
+function mapStateToProps({ questions, authUser }) {
+  return {
+    authUser,
+    questionId: Object.keys(questions).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    ),
+    questions,
+  };
+}
+
+export default connect(mapStateToProps)(AnsweredQuestionList);
