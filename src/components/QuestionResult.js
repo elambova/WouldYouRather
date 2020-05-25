121bd39359098cb6314a1c68534b41616c9963a1
Elena Lambova <elena.g.lambova@gmail.com>
2020-05-25 17:30:00 +0300

    Feat: Add badge with YOUR VOTE is.

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Header, Image, Progress, Divider } from "semantic-ui-react";
+import { Header, Image, Progress, Divider, Label } from "semantic-ui-react";
 
 class QuestionResult extends Component {
   render() {
     const id = this.props.location.pathname.slice(17);
-    const { questions, users } = this.props;
+    const { questions, users, authUser } = this.props;
 
     const { optionOne, optionTwo, author } = questions[id];
     const { name, avatarURL } = users[questions[id].author];
@@ -21,11 +21,19 @@ class QuestionResult extends Component {
           <div className="description">
             <Header as="h3">Results:</Header>
             <div className="border padding">
+              {optionOne.votes.includes(authUser) ? (
+                <Label as="a" color="teal" ribbon="right">
+                  Your Vote
+                </Label>
+              ) : null}
               <p className="options">{optionOne.text}</p>
               <Progress
                 className="progress-bar-margin"
                 progress
-                percent={(optionOne.votes.length / allVotes.length) * 100}
+                percent={(
+                  (optionOne.votes.length / allVotes.length) *
+                  100
+                ).toFixed(2)}
               />
               <p className="votes">
                 {optionOne.votes.length} out of {allVotes.length} votes
@@ -33,11 +41,19 @@ class QuestionResult extends Component {
             </div>
             <Divider />
             <div className="border padding">
+              {optionTwo.votes.includes(authUser) ? (
+                <Label as="a" color="teal" ribbon="right">
+                  Your Vote
+                </Label>
+              ) : null}
               <p className="options">{optionTwo.text}</p>
               <Progress
                 className="progress-bar-margin"
                 progress
-                percent={(optionTwo.votes.length / allVotes.length) * 100}
+                percent={(
+                  (optionTwo.votes.length / allVotes.length) *
+                  100
+                ).toFixed(2)}
               />
               <p className="votes">
                 {optionTwo.votes.length} out of {allVotes.length} votes
