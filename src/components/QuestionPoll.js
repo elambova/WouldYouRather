d4de29801023cde2d3ca6d19b66c32f48fa5f8ab
Elena Lambova <elena.g.lambova@gmail.com>
2020-05-25 16:46:17 +0300

    Feat: Add QuestionPoll.js Component.

---
+++ src/components/QuestionPoll.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Image, Form, Radio, Header } from "semantic-ui-react";
+import { handleVote } from "../actions/shared";
+import { Redirect } from "react-router-dom";
+
+class QuestionPoll extends Component {
+  state = {
+    value: "",
+    toResult: false,
+  };
+
+  handleChange = (e, { value }) => this.setState({ value });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { value } = this.state;
+
+    const { dispatch, authUser } = this.props;
+    const id = this.props.match.params.id;
+    dispatch(handleVote(authUser, id, value));
+
+    this.setState(() => ({
+      value: "",
+      toResult: true,
+    }));
+    console.log(id);
+  };
+
+  render() {
+    const { value, toResult } = this.state;
+    if (toResult === true) {
+      return <Redirect to="/question_result" />;
+    }
+
+    const id = this.props.location.pathname.slice(10);
+    const { questions, users } = this.props;
+
+    const disabled = value === "" ? true : false;
+    if (questions[id] === undefined) {
+      return <Redirect to="/404" />;
+    }
+
+    const { optionOne, optionTwo, author } = questions[id];
+    const { name, avatarURL } = users[questions[id].author];
+
+    return (
+      <div className="question border">
+        <Header as="h2">{name} asks:</Header>
+        <div className="question-content">
+          <div className="image-content">
+            <Image src={avatarURL} alt={author} />
+          </div>
+          <div className="description">
+            <Header as="h3">Would you rather ...</Header>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group grouped>
+                <Form.Field
+                  control={Radio}
+                  label={optionOne.text}
+                  value="optionOne"
+                  checked={value === "optionOne"}
+                  onChange={this.handleChange}
+                />
+                <Form.Field
+                  control={Radio}
+                  label={optionTwo.text}
+                  value="optionTwo"
+                  checked={value === "optionTwo"}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Button
+                content="Submit"
+                color="teal"
+                disabled={disabled}
+                fluid
+              />
+            </Form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+function mapStateToProps({ questions, users, authUser }) {
+  return {
+    questions,
+    users,
+    authUser,
+  };
+}
+export default connect(mapStateToProps)(QuestionPoll);
